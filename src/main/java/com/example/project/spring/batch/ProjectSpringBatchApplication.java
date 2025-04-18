37b426e64bf1bce967974b9ca620053e69a3fcd1
angelo7890 <ar293978@gmail.com>
2025-04-18 01:34:33 -0300

    iniciando projeto

---
+++ src/main/java/com/example/project/spring/batch/ProjectSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.project.spring.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project/spring/batch/ProjectSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
