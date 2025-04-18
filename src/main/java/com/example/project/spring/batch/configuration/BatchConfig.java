7f2ed095cac90cf11ffb01a934f8be52b2d972a4
angelo7890 <ar293978@gmail.com>
2025-04-18 02:11:47 -0300

    criacao da configuracao de job e step

--- src/main/java/com/example/project/spring/batch/configuration/BatchConfig.java
+++ src/main/java/com/example/project/spring/batch/configuration/BatchConfig.java
@@ -5,6 +5,10 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -12,10 +16,24 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 public class BatchConfig {
     private final PlatformTransactionManager transactionManager;
+    private final JobRepository jobRepository;
 
 
-    public BatchConfig(PlatformTransactionManager transactionManager) {
+    public BatchConfig(PlatformTransactionManager transactionManager, JobRepository jobRepository) {
         this.transactionManager = transactionManager;
+        this.jobRepository = jobRepository;
     }
-    
+
+    @Bean
+    public Job job(Step step) {
+        return new JobBuilder("job", jobRepository)
+                .start(step).build();
+    }
+
+    @Bean
+    public Step step(ItemReader reader, ItemProcessor processor , ItemWriter writer) {
+        return new StepBuilder("step", jobRepository).chunk(100, transactionManager)
+                .reader(reader).processor(processor).writer(writer).build();
+
+    }
 }
